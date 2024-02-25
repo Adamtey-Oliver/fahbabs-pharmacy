fa9f3627c2cd27dffad2083d6d7216688a5391b7
Lezcano Tech  <153436452+Oliver-Adamtey@users.noreply.github.com>
2024-02-25 11:45:22 +0000

    Update LoginViews.java

--- src/main/java/dev/danvega/hellovaadin/login/LoginViews.java
+++ src/main/java/dev/danvega/hellovaadin/login/LoginViews.java
@@ -1,14 +1,10 @@
 package dev.danvega.hellovaadin.login;
 
 import com.vaadin.flow.component.Composite;
-import com.vaadin.flow.component.login.LoginForm;
+import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.login.LoginOverlay;
-import com.vaadin.flow.component.orderedlayout.FlexComponent;
-import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-import com.vaadin.flow.component.textfield.EmailField;
+import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.router.Route;
-
-import java.awt.*;
 
 @Route("login")
 public class LoginViews extends Composite<LoginOverlay> {
@@ -16,23 +12,19 @@ public class LoginViews extends Composite<LoginOverlay> {
     public LoginViews(){
         LoginOverlay loginOverlay = getContent();
         loginOverlay.setTitle("Fahbabs");
+        loginOverlay.setDescription("Pharmacy Management System");
         loginOverlay.setOpened(true);
 
-//        TextField firstName = new TextField("First name");
-//        TextField lastName = new TextField("Last name");
-//        EmailField email = new EmailField("email");
-//
-//        from(){
-//            var saveButton = new Button("Save");
-//            add(firstName, lastName, email, saveButton);
-//        };
+        loginOverlay.addLoginListener(event -> {
+            if ("user".equals(event.getUsername())) {
+                UI.getCurrent().navigate("user");
+            }else if ("admin".equals(event.getUsername())){
+                UI.getCurrent().navigate(AdminView.class);
+            } else {
+                Notification.show("Wrong credentials");
+            }
+        });
+
     }
 
-//    public LoginViews() {
-//        VerticalLayout layout = getContent();
-//        layout.add(
-//                new LoginForm()
-//        );
-//        layout.setAlignItems(FlexComponent.Alignment.CENTER);
-//    }
 }
